1eccb6f757d26cb8a3f65cad6b699476024514dd
Haosheng Zhang <zhanghaosheng2000@163.com>
2023-01-18 22:06:17 -0500

    update to create database

--- Database/createCourseDatabase.sql
+++ Database/createCourseDatabase.sql
@@ -3,11 +3,12 @@ CREATE DATABASE coursedatabase;
 SET SQL_SAFE_UPDATES = 0;
 set sql_mode='no_auto_create_user,no_engine_substitution';
 
-DROP TABLE IF EXISTS course_main;
-CREATE TABLE course_main(
+DROP TABLE IF EXISTS courses;
+CREATE TABLE courses(
 	course_id VARCHAR(255),
     section INT,
     total_slots INT,
+    currently_enrolled INT,
     department VARCHAR(255),
     credit Double,
     course_time VARCHAR(255),
@@ -16,7 +17,10 @@ CREATE TABLE course_main(
     PRIMARY KEY(course_id, section)
 );
 
-CREATE TABLE students(
-	student_id VARCHAR(255),
-    PRIMARY KEY(student_id)
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
+	user_id VARCHAR(255),
+    user_name VARCHAR(255),
+	user_type INT,
+    PRIMARY KEY(user_id)
 );
