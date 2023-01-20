23d8e3e7dec9955a9d18c202bc06f887e1e4b62a
Haosheng Zhang <zhanghaosheng2000@163.com>
2023-01-19 20:50:20 -0500

    added some new procedures

--- Database/procedures.sql
+++ Database/procedures.sql
@@ -53,3 +53,64 @@ END //
 
 DELIMITER ;
 
+DELIMITER //
+
+CREATE PROCEDURE updatecourse(IN course_id VARCHAR(255),
+    IN course_name VARCHAR(255),
+    IN section INT,
+    IN total_slots INT,
+    IN currently_enrolled INT,
+    IN department VARCHAR(255),
+    IN credit Double,
+    IN course_time VARCHAR(255),
+    IN instructor VARCHAR(255),
+    IN description TEXT)
+BEGIN
+	START TRANSACTION; 
+		UPDATE courses
+		SET courses.course_name = course_name, 
+        courses.total_slots = total_slots, 
+        courses.currently_enrolled = currently_enrolled,
+        courses.department = department,
+        courses.credit = credit,
+        courses.course_time = course_time,
+        courses.instructor = instructor,
+        courses.description = description
+        WHERE courses.course_id = course_id AND courses.section = section;
+		COMMIT;
+		SELECT 'UPDATE SUCCESSFUL!';
+END //
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE addcourses(IN course_id VARCHAR(255), IN section INT)
+BEGIN
+    
+	START TRANSACTION;
+		INSERT INTO courses
+        VALUES(course_id, section);
+        
+		COMMIT;
+		SELECT 'ADD SUCCESSFUL!';
+
+END //
+
+DELIMITER ;
+
+
+
+DELIMITER //
+CREATE TRIGGER nullcourseid
+BEFORE INSERT
+ON courses
+FOR EACH ROW
+
+	IF NEW.course_id IS NULL OR NEW.section IS NULL THEN
+		SIGNAL	SQLSTATE'22003'
+		SET	MESSAGE_TEXT = 'Course id and section cannot be null';
+END IF;
+//
+DELIMITER ;
+
